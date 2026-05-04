05c7e678d2c7d93e0a14c7244f84ad0b640ab6c4
quarkusio <no-reply@quarkus.io>
2026-05-04 12:16:13 +0000

    Initial commit

---
+++ src/test/java/org/leonardo/quarkusconcurrencylab/GreetingResourceIT.java
@@ -0,0 +1,8 @@
+package org.leonardo.quarkusconcurrencylab;
+
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+
+@QuarkusIntegrationTest
+class GreetingResourceIT extends GreetingResourceTest {
+    // Execute the same tests but in packaged mode.
+}
